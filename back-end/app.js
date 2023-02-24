3598fd6dccd9f8174b40e7b5970b551634c3ce24
Luca <lucamaia2007@hotmail.com>
2023-02-24 15:12:12 -0300

    first commit

---
+++ back-end/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use((req,res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", 'X-PINGOTHER, Content-Type, Authorization');
+    app.use(cors());
+    next();
+});
+
+app.get('/', async(req,res) => {
+    return res.json({
+        erro: false,
+        datahome: {
+            text_one:"Exemplo 123",
+            text_two:"Exemplo 2",
+            text_three:"Exemplo 3",
+        }
+    })
+})
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080")
+})
